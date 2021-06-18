3956c0ea76fdefeeeac09b3592b8d9ee6e974cc1
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-19 02:21:50 +0300

    aauthenticate user

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db=SQLAlchemy()
-
+mail = Mail()
 def create_app(config_name):
     app = Flask(__name__)
 
@@ -16,6 +17,7 @@ def create_app(config_name):
     #initialize extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
 
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from . import main
-from flask import render_template
+from flask import render_template,redirect, url_for,abort
+from flask_login import login_required, current_user
+
+from .. import db
 
 @main.route('/')
 def index():
-    
-    return render_template('index.html')
+
+    return render_template('index.html' , current_user=current_user)
